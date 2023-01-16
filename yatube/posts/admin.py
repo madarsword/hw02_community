40a9592bd05eae3f867dfe5bf6cca46e35f5738f
Александр Шпара <aleksandrshpr@yandex.ru>
2023-01-16 14:21:47 +0100

    текст комментария

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -8,11 +9,11 @@ class PostAdmin(admin.ModelAdmin):
         'pub_date',
         'author',
         'group'
-    )
-    list_editable = ('group',) 
-    search_fields = ('text',) 
+)
+    list_editable = ('group',)
+    search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'  
+    empty_value_display = '-пусто-'
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)  
+admin.site.register(Group)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-User = get_user_model() 
+User = get_user_model()
 
 
 class Group(models.Model):
@@ -48,4 +48,4 @@ class Post(models.Model):
 
 
 class Meta:
-        ordering = ['-pub_date']
+    ordering = ['-pub_date']
