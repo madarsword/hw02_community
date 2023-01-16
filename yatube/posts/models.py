d25b2f6876d4d402963562db7d08defde2f107b0
Александр Шпара <aleksandrshpr@yandex.ru>
2023-01-16 14:11:27 +0100

    текст комментария

---
+++ yatube/posts/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model() 
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Группа'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Адрес страницы с постами группы'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+
+    def __str__(self):
+        return f'Группа "{self.title}"'
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='Текст'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    author = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name="Группа"
+    )
+
+
+class Meta:
+        ordering = ['-pub_date']
